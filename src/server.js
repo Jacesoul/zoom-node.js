19c986af93c02df7046cada927699fed88ff00a9
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-12-10 15:14:09 +0900

    HTTP vs WebSocket

--- src/server.js
+++ src/server.js
@@ -5,8 +5,8 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/")); // 다른 URL을 사용하지 않고 오직 home URL만 사용
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/")); // 다른 URL을 사용하지 않고 오직 home URL만 사용
 
 const PORT = 3000;
 const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
