f1fbf45dfa4e671298cfc0e670ead62ea1c68a9b
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-12-10 14:55:52 +0900

    RECAP

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/")); // 다른 URL을 사용하지 않고 오직 home URL만 사용
 
 const PORT = 3000;
 const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
